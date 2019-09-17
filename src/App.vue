f54977f272fbd8985026c3351da0514f65d91987
Max Krak <krakenslab@gmail.com>
2019-09-18 01:20:04 +0300

    added PlaceInput

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <PlaceInput/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PlaceInput from './components/PlaceInput.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    PlaceInput
   }
 }
 </script>
---
+++ src/components/PlaceInput.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="place-input">
+    <label for="slug">Слаг заведения</label>
+    <input type="text" v-model="slug" id="slug">
+    <button @click="onGetSchedules(slug)">Обновить</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'PlaceInput',
+  props: {
+    msg: String
+  },
+  data: function () {
+    return {
+      slug: ''
+    }
+  },
+  methods: {
+    onGetSchedules: function(slug) {
+      if (typeof(slug) === 'string' && slug !== '') {
+        alert(slug)
+      }
+    }
+  }
+}
+</script>
