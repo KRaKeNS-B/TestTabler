a1bb2a86cadc7ea2141515fe5b14ea7f7bf945f1
KRaKeNS <krakenslab@gmail.com>
2019-09-18 15:48:47 +0300

    added the definition state place

--- src/components/Schedule.vue
+++ src/components/Schedule.vue
@@ -8,7 +8,7 @@
       {{schedule.isMain ? workHours : schedule.name}}
     </div>
     <div class="schedule-card__state">
-      {{new Date().getDay()}}
+      {{isActive()}}
     </div>
   </div>
 </template>
@@ -19,7 +19,39 @@ export default {
   props: ['schedule'],
   data () {
     return {
-      workHours: 'Часы работы' // TODO add the more info wrapper
+      workHours: 'Часы работы',
+      placeState: {
+        open: 'открыто',
+        closed: 'закрыто'
+      }
+    }
+  },
+  methods: {
+    isActive () {
+      let now = Date.now()
+      let item = this.getCurrentDay(new Date().getDay())
+
+      if (item !== false) {
+        if (this.getFullDateTime(item.startAt) <= now && this.getFullDateTime(item.endAt) >= now) {
+          return this.placeState.open
+        }
+      }
+
+      return this.placeState.closed
+    },
+    getCurrentDay (day) {
+      day = day === 0 ? 6 : day - 1
+      for (let item of this.schedule.items) {
+        if (item.dayOfWeek === day) {
+          console.log(day)
+          return item
+        }
+      }
+      // Date.parse(new Date().toDateString() + ' ' + '22:00')
+      return false
+    },
+    getFullDateTime (time) {
+      return Date.parse(new Date().toDateString() + ' ' + time)
     }
   }
 }
